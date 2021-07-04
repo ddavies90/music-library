876e2cd087612da053098b3337cb7c864d57603a
David Davies <d.davies90@hotmail.co.uk>
2021-07-04 21:43:44 +0100

    Added controller for artist create method

---
+++ src/controllers/artist.js
@@ -0,0 +1,14 @@
+const getDb = require('../services/db');
+
+exports.create = async (req, res) => {
+    const db = await getDb();
+    const { name, genre } = req.body;
+
+    try {
+        await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
+        res.status(201).json(req.body);
+    } catch (err) {
+        res.sendStatus(500).json(err);
+    }
+    db.close();
+};
