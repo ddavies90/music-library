59ba791745efe38d940339ed2d19ef43ed7c1d88
David Davies <d.davies90@hotmail.co.uk>
2021-07-05 21:07:49 +0100

    Added controller functions for databse CRUD operations

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -5,10 +5,79 @@ exports.create = async (req, res) => {
     const { name, genre } = req.body;
 
     try {
-        await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
+        await db.query(`INSERT INTO Artist (name, genre) VALUES (?, ?)`, [
+            name,
+            genre
+        ]);
         res.status(201).json(req.body);
     } catch (err) {
-        res.sendStatus(500).json(err);
+        res.Status(500).json(err);
+    }
+    db.close();
+};
+
+exports.read = async (__, res) => {
+    const db = await getDb();
+
+    try {
+        const [artists] = await db.query('SELECT * FROM Artist');
+        res.status(200).send(artists);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    db.close();
+};
+
+exports.getById = async (req, res) => {
+    const db = await getDb();
+    const { id } = req.params;
+
+    try {
+        const [[artist]] = await db.query('SELECT * FROM Artist WHERE id = ?', [id]);
+        if (!artist) {
+            res.sendStatus(404);
+        } else {
+            res.status(200).json(artist);
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    db.close();
+};
+
+exports.update = async (req, res) => {
+    const db = await getDb();
+    const { id } = req.params;
+    const data = req.body;
+
+    try {
+        const [[artist]] = await db.query('SELECT * FROM Artist WHERE id = ?', [id]);
+        if (!artist) {
+            res.sendStatus(404);
+        } else {
+            await db.query('UPDATE Artist SET ? WHERE id = ?', [data, id]);
+            res.sendStatus(200);
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    db.close();
+};
+
+exports.delete = async (req, res) => {
+    const db = await getDb();
+    const { id } = req.params;
+
+    try {
+        const [[artist]] = await db.query('SELECT * FROM Artist WHERE id = ?', [id]);
+        if (!artist) {
+            res.sendStatus(404);
+        } else {
+            await db.query('DELETE FROM Artist WHERE id = ?', [id]);
+            res.sendStatus(200);
+        }
+    } catch (err) {
+        res.status(500).send(err);
     }
     db.close();
 };
