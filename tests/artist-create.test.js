c2c19d62bdbf1369476e2df08a4d90f3330e4c54
David Davies <d.davies90@hotmail.co.uk>
2021-07-05 21:03:26 +0100

    Added tests for API CRUD operations to database

--- tests/artist-create.test.js
+++ tests/artist-create.test.js
@@ -4,32 +4,31 @@ const getDb = require('../src/services/db');
 const app = require('../src/app');
 
 describe('create artist', () => {
-let db;
-beforeEach(async () => (db = await getDb()));
+    let db;
+    beforeEach(async () => (db = await getDb()));
 
-afterEach(async () => {
-    await db.query('DELETE FROM Artist');
-    await db.close();
-});
+    afterEach(async () => {
+        await db.query('DELETE FROM Artist');
+        await db.close();
+    });
 
-describe('/artist', () => {
-    describe('POST', () => {
-    it('creates a new artist in the database', async () => {
-        const res = await request(app).post('/artist').send({
-        name: 'Tame Impala',
-        genre: 'rock',
+    describe('/artist', () => {
+        describe('POST', () => {
+            it('creates a new artist in the database', async () => {
+                const res = await request(app).post('/artist').send({
+                    name: 'Tame Impala',
+                    genre: 'rock',
+                });
+
+                expect(res.status).to.equal(201);
+
+                const [[artistEntries]] = await db.query(
+                    `SELECT * FROM Artist WHERE name = 'Tame Impala'`
+                );
+
+                expect(artistEntries.name).to.equal('Tame Impala');
+                expect(artistEntries.genre).to.equal('rock');
+            });
         });
-
-        expect(res.status).to.equal(201);
-
-        const [[artistEntries]]§ = await db.query(
-        `SELECT * FROM Artist WHERE name = 'Tame Impala'`
-        );
-        console.log(artistEntries);
-
-        expect(artistEntries.name).to.equal('Tame Impala');
-        expect(artistEntries.genre).to.equal('rock');
     });
-    });
-});
 });
