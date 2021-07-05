4d514806388ddda91f521772a624e13d6d65679e
David Davies <d.davies90@hotmail.co.uk>
2021-07-05 21:08:20 +0100

    Created routes for CRUD operations on the Artist table

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const artistController = require('../controllers/artist');
 
-router.post('/', artistController.create);
+router.route('/')
+    .post(artistController.create)
+    .get(artistController.read)
+
+router.route('/:id') 
+    .get(artistController.getById)
+    .patch(artistController.update)
+    .delete(artistController.delete)
 
 module.exports = router;
