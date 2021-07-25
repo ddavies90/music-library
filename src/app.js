82cfa604ea7b06a6478ac181c6f05fe6dc0acdff
David Davies <d.davies90@hotmail.co.uk>
2021-07-25 15:22:20 +0100

    Added cors to app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDoc = require('../swagger.json');
 const artistRouter = require('./routes/artists');
 const albumRouter = require('./routes/albums');
+
+app.use(cors());
 
 app.use(express.json());
 
